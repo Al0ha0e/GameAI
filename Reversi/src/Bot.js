b752d408dda630dfa075a04e4ad9a23313fb1c89
Al0ha0e <1919270783@qq.com>
2018-10-21 22:39:40 +0800

    normal AI

--- Reversi/src/Bot.js
+++ Reversi/src/Bot.js
@@ -5,6 +5,16 @@ class bot
         this.gameBase = gamebase;
         this.difficulty = difficulty;
         this.normalMaxDeep = 5;
+        this.evaluateTable = [
+            [90,-60,10,10,10,10,-60,90],
+            [-60,-80,5,5,5,5,-80,-60],
+            [10,5,1,1,1,1,5,10],
+            [10,5,1,1,1,1,5,10],
+            [10,5,1,1,1,1,5,10],
+            [10,5,1,1,1,1,5,10],
+            [-60,-80,5,5,5,5,-80,-60],
+            [90,-60,10,10,10,10,-60,90]
+        ];
     }
     botMove()
     {
@@ -66,32 +76,88 @@ class bot
     }
     normalStrategy()
     {
-        
+        let evaluateFunction = (currentBoard,currentColor)=>{
+            let ret = 0;
+            for(let i=0;i<8;i++)
+            {
+                for(let j=0;j<8;j++)
+                {
+                    if(currentBoard[i][j]==currentColor){ret += this.evaluateTable[i][j];}
+                    else if(currentBoard[i][j]==(currentColor^1)){ret -= this.evaluateTable[i][j];}
+                } 
+            }
+            return ret;
+        };
+        let l = -100000000;
+        let maxres = -100000000;
+        let x,y;
+        for(let i=0;i<8;i++)
+        {
+            for(let j=0;j<8;j++)
+            {
+                if(this.gameBase.digitalBoard.canSet(i,j,this.gameBase.currentColor))
+                {
+                    let ano = new board(this.gameBase.digitalBoard);
+                    ano.set(i,j,this.currentColor);
+                    let res = this.Search(ano,this.gameBase.currentColor^1,2,6,evaluateFunction,l,100000000);
+                    if(res>maxres)
+                    {
+                        maxres = res;
+                        x = i; y = j;
+                    }
+                    l = Math.max(l,res);
+                }
+            }
+        }
+        this.gameBase.digitalBoard.set(x,y,this.gameBase.currentColor);
     }
     hardStrategy()
     {
 
     }
-    Search(presentBoard,presentColor,deep,maxdeep,evaluateFunction,l,r)
+    Search(currentBoard,currentColor,deep,maxdeep,evaluateFunction,l,r)
     {
-        let maxres = evaluateFunction(presentBoard.digitalBoard);
-        if(presentBoard.canNotSet(presentColor)){ return (deep%2==1) ? -maxres : maxres;}
+        //console.log(deep+" "+l+" "+r);
+        let maxres; 
+        if(currentBoard.canNotSet(currentColor))
+        {
+            maxres = evaluateFunction(currentBoard.digitalBoard,currentColor);
+            return (deep%2==1) ? -maxres : maxres;
+        }
         if(deep==maxdeep)
         {
+            maxres = evaluateFunction(currentBoard.digitalBoard,currentColor);
             for(let i=0;i<8;++i)
             {
                 for(let j=0;j<8;++j)
                 {
-                    if(presentBoard.canSet(i,j,presentColor))
+                    if(currentBoard.canSet(i,j,currentColor))
                     {
-                        let ano = new board(presentBoard);
+                        let ano = new board(currentBoard);
                         ano.set(i,j,currentColor);
-                        maxres = Math.max(evaluateFunction(ano.digitalBoard),maxres);
+                        maxres = Math.max(evaluateFunction(ano.digitalBoard,currentColor),maxres);
                     }
                 }
             }
             return (deep%2==1) ? -maxres : maxres;
         }
-        
+        maxres = -100000000;
+        for(let i=0;i<8;++i)
+        {
+            for(let j=0;j<8;++j)
+            {
+                if(currentBoard.canSet(i,j,currentColor))
+                {
+                    let ano = new board(currentBoard);
+                    ano.set(i,j,currentColor);
+                    let res = this.Search(ano,currentColor^1,deep+1,maxdeep,evaluateFunction,l,r);
+                    maxres = Math.max(maxres,res);
+                    if((deep%2)==1){l = Math.max(l,res);}
+                    else{r = Math.min(r,-res);}
+                    if(r<l) return -maxres;
+                }
+            }
+        }
+        return -maxres;
     }
 }
